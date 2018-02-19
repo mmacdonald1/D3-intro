2110a8e6481912b201c1b17e7af4b6b00d83c0d0
Melanie MacDonald <melmacd94@gmail.com>
2018-02-18 20:32:59 -0500

    manipulating selections

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//SELECTION NOTES---------------------------------------------
 // The select method will only grab the first element it finds 
 
 //d3.select('p');
@@ -6,11 +7,23 @@
 
 //d3.selectAll('p');
 
-var el = d3.select('body').append('p').attr('class','foo').text('Hello World');
-
 //Shows a selection. A selection is a javascript object that represents an HTML element on the browser.
-console.log(el);
+
 
 //d3.selection library https://github.com/d3/d3-selection -- allows you to select and change html elements
 
 //A node is the technical term for everything and anything in your document
+
+//MANIPULATING SELECTIONS--------------------------------------
+//transformation methods are just functions that change how an element appears or works
+
+//cannot add more than one class using attr('class','foo') use classed('foo', true)
+
+var el = d3.select('body')
+.append('p')
+.classed('foo', true)
+.classed('bar', true)
+.text('Hello World')
+.style('color', 'blue');
+
+console.log(el);
