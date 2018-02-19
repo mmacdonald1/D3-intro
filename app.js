ac47f63cac1add4721b7298fe87c755829405912
Melanie MacDonald <melmacd94@gmail.com>
2018-02-18 21:29:10 -0500

    loading data from an external source

--- app.js
+++ app.js
@@ -35,8 +35,64 @@
 
 //DISPLAYING DATA----------------------------------------------
 
+//
+//var dataset=[10,20,30,40,50];
+//
+//var el = d3.select('body')
+//.selectAll('p')
+//.data(dataset)
+//.enter()
+//.append('p')
+//.text(function(d){
+//    return 'This paragraph is binded to the number '+d;
+//})
+//.attr('class', function(d){
+//  if(d<25){
+//    return 'foo';
+//}else{
+//      return null;
+//}})
+//.classed('bar', function(d){
+//    return d<25; // 10< 25 =true
+//})
+//.style('color',function(d){
+//    if(d>25){
+//        return 'red';
+//    }else{
+//        return 'blue';
+//    }
+//})
+//;
 
-var dataset=[10,20,30,40,50];
+//console.log(el);
+
+//LOADING DATA FROM AN EXTERNAL SOURCE
+// d3 request library https://github.com/d3/d3-request
+// 3 supported data formats for d3 text, CSV, JSON
+// Google spreadsheets and excel are csv
+//Error first functions are functions that will always be passed in an error as the first argument
+//to view the speed of AJAX calls go to the network and xhr tabs
+//click the call to see preview 
+//d3 logic will load faster than the data so
+
+d3.csv('data.csv', function(err, data){
+   if(err){
+       return console.log(err);
+   } 
+    console.log(data);
+//    generate(data.columns);
+});
+
+//d3.json('data.json', function(err, data){
+//    if(err){
+//        return console.log(err);
+//    }
+//    console.log(data);
+//    generate(data);
+//});
+
+function generate(dataset){
+//var dataset=[10,20,30,40,50];
 
 var el = d3.select('body')
 .selectAll('p')
@@ -63,5 +119,4 @@ var el = d3.select('body')
     }
 })
 ;
-
-console.log(el);
+}
